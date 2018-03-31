0f380be0dcff373427b86446658aa1ffe1d08443
Ryan Jacobs <ryan.mjacobs@gmail.com>
2018-03-31 12:29:43 -0700

    add github.js

---
+++ github.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+// webserver
+const Koa = require("koa");
+const bodyParser = require("koa-bodyparser");
+const app = new Koa();
+app.use(bodyParser());
+
+const {exec} = require("child_process");
+
+app.use(async (ctx, next) => {
+    console.log(ctx.method, ctx.url);
+    await next();
+});
+
+app.use(async ctx => {
+    const p = ctx.request.body;
+    const req = ctx.request.method + ctx.url;
+
+    if (p.repository == "getmeramen" && p.commits.length > 0) {
+        exec("git pull", (err, stdout, stderr) => {
+            if (err) return;
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+        });
+    }
+});
+
+app.listen(1251, function() {
+    console.log("listening http://0.0.0.0:1251");
+});
