e6c350c885a576228e67b9b06a0bf510972af0b3
Ryan Jacobs <ryan@rmj.us>
2018-03-31 14:19:39 -0700

    wip navi

--- App.js
+++ App.js
@@ -1,6 +1,34 @@
 import React from "react";
 import { StyleSheet, Text, View, Image, Button } from "react-native";
 
+import {
+  StackNavigator,
+} from "react-navigation";
+
+
+const App = StackNavigator({
+  Home: { screen: HomeScreen },
+  Profile: { screen: ProfileScreen },
+});
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <Button
+        title="Go to Jane's profile"
+        onPress={() =>
+          navigate('Profile', { name: 'Jane' })
+        }
+      />
+    );
+  }
+}
+
+/*
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -26,6 +54,8 @@ export default class App extends React.Component {
         <Text style={{fontSize: 200}}>{this.state.count}</Text>
 
         <Button onPress={this.click} title="now"/>
+
+        <Navi
       </View>
     );
   }
@@ -39,3 +69,4 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   }
 });
+*/
